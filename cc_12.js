6e0fdd620965999e98a9926841851a0b37138a2b
ssj2-z <ssj2@usf.edu>
2025-03-02 17:14:39 -0500

    "Task 3 - Implemented Dynamic Inventory List"

--- cc_12.js
+++ cc_12.js
@@ -21,3 +21,44 @@ metricCardsArray.forEach(function(card) {
     card.innerHTML += ` - Updated`;
     card.computedStyleMap.backgroundColor = `#f0f0f0`;
 });
+
+// Task 3 Adding and Removing Items
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,
+initial-scale=1.0">
+    <title>Inventory Management</title</title>
+</head>
+<body>
+    <ul id="inventoryList"></ul>
+    <button onclick="addProduct()">Add Product</button>
+
+    <script>
+        function addProduct() {
+            var newProduct = document.createElement(`li`);
+
+            newProduct.setAttribute(`class`, `product-item`);
+            newProduct.setAttribute(`data-product-id`,
+`product-` + Date.now());
+
+            newProduct.innerText = `New Product`;
+            var inventoryList =
+document.getElementById(`inventoryList`);
+            inventoryList.appendChild(newProduct);
+
+            newProduct.addEventListener(`click`, function() {
+                removeProduct(newProduct);
+            });
+            }
+            
+            function removeProduct(productItem) {
+                var inventoryList = 
+document.getElementById(`inventoryList`);
+                inventoryList.removeChild(productItem);
+            });
+
+    </script>
+</body>
+</html>
